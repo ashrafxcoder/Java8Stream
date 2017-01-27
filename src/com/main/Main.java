0dfc8a4f2b492a7b2b47657d7c6fa979cc44538a
Ashraf Ali <ashraf.ali1811@gmail.com>
2017-01-27 19:25:42 +0500

    Consumer functional interface tests added

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -7,7 +7,9 @@ package com.main;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FilenameFilter;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
 import java.util.Spliterator;
@@ -26,37 +28,35 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        showDirs(new File("F:\\Java"));
+        
+        Comparator<Integer> comp = Integer::compare;
+        
+        //showDirs(new File("F:\\Java"));
+        consumerTests();
+        
     }
 
     private static void streamsIntro() {
-        BinaryOperator<Integer> result = (x,y) -> x+y;
-        
+        BinaryOperator<Integer> result = (x, y) -> x + y;
+
         new Random().ints().limit(10).forEach(i -> System.err.println(i));
-        
-        
+
         Function<String, String> upCase = name -> name.toUpperCase();
-        
+
         Consumer<String> show = s -> System.err.println(s);
-        
+
         List<String> names = Arrays.asList(new String[]{"ashraf", "umair", "hassan", "rasheed"});
-        
+
         if (names.stream().anyMatch(s -> s.startsWith("um"))) {
             System.out.println("Umair is present");
         }
-        
-        Spliterator.OfInt spliterator = Arrays.spliterator(new int[] {1,2,3,4,5,6,7,8,9,10});
-        
-        
-        
+
+        Spliterator.OfInt spliterator = Arrays.spliterator(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+
         System.out.println(upCase.apply("ashraf"));
-        
+
         show.accept("hi");
-        
-        
-        
-        
-        
+
         StringJoiner joiner = new StringJoiner(",", "[", "]");
         joiner.add("kotlin");
         joiner.add("phantom");
@@ -64,14 +64,27 @@ public class Main {
         joiner.add("groovy");
         joiner.add("scala");
         System.out.println(joiner.toString());
-        
-        
+
         System.out.println(result.apply(2, 3));
     }
 
-    
-    public static void showDirs(File dir){
+    public static void showDirs(File dir) {
         FileFilter filter = (p) -> p.getName().endsWith(".java");
-        Arrays.asList(dir.listFiles(filter)).forEach(System.out::println);
+        FilenameFilter nameFilter = (d ,n) -> n.endsWith(".java");
+
+        Arrays.asList(dir.list())
+                .forEach(System.out::println);
+    }
+    
+    
+    
+    public static void consumerTests(){
+        Consumer<Integer> print = System.out::println;
+        Consumer<Integer> doubleIt = i -> i *= 2;
+        
+        
+        List<Integer> ints = Arrays.asList(new Integer[]{1,2,3,4,5,6,7,8,9,10});
+        
+        ints.forEach(print.andThen(doubleIt).andThen(print));
     }
 }
