a310d9579be1b08ea10a61df6ee22d60bec7e7b5
Ashraf Ali <ashraf.ali1811@gmail.com>
2017-01-28 18:52:35 +0500

    countWords method added which is fully implemented using stream API

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -22,14 +22,15 @@ import java.util.List;
 import java.util.Random;
 import java.util.Spliterator;
 import java.util.StringJoiner;
+import java.util.TreeMap;
 import java.util.function.BinaryOperator;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.PersistenceUnit;
 
 /**
  *
@@ -40,31 +41,45 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    
-    private static EntityManagerFactory emf 
-                = Persistence.createEntityManagerFactory("WorldPU"); 
-    public static void main(String[] args) throws Exception{
-        
+    private static EntityManagerFactory emf
+            = Persistence.createEntityManagerFactory("WorldPU");
+
+    public static void main(String[] args) throws Exception {
+
         Comparator<Integer> comp = Integer::compare;
-        
-        
+
         SecurityManager manager;
-       
-        
+
         CityJpaController city = new CityJpaController(emf);
-        
-        //List<City> cities = city.findCityEntities();
-       
+
+        List<City> cities = city.findCityEntities();
+
+        long worldPopulation = cities.stream()
+                .mapToLong(City::getPopulation)
+                .sum();
+                                    //.reduce(0, (x,y) -> x+y);
+
+        System.out.println(worldPopulation);
+
+        //Map<String, List<City> map =
+        TreeMap<String, List<City>> map = cities
+                .stream()
+                .filter(c -> c.getCountryCode().contains("PAK"))
+                .collect(Collectors.groupingBy(
+                                City::getCountryCode,
+                                TreeMap::new, Collectors.toList()
+                        ));
+        map.forEach((s, l) -> {
+            System.out.printf("%s has %d citie(s)%n", s, l.size());
+            l.forEach(c -> System.out.println(c.getName() + " " + c.getDistrict()));
+        });
+
         //cities.forEach(c -> System.out.println(c.getName()));
-        
-        
         CountryJpaController countries = new CountryJpaController(emf);
         List<Country> CountryEntities = countries.findCountryEntities();
-        
-        CountryEntities.forEach(c -> System.out.println(c.getCode()));
-        System.out.println(CountryEntities.stream().count());
-        
-        
+
+        //CountryEntities.forEach(c -> System.out.println(c.getCode()));
+        //System.out.println(CountryEntities.stream().count());
         //walkDirs();
         //StreamCollectors collectors = new StreamCollectors();
         //collectors.collectPerson();
@@ -72,29 +87,26 @@ public class Main {
         //test.flatMap();
         //showDirs(new File("F:\\Java"));
         //consumerTests();
-        
     }
 
-    
-    
     public static void listDirs() throws Exception {
         Path path = Paths.get("G:", "University");
-        try(Stream<Path> stream = Files.list(path)){
+        try (Stream<Path> stream = Files.list(path)) {
             stream
-                  .filter(p -> p.toFile().getName().endsWith(".pdf"))
-                  .forEach(System.out::println);
+                    .filter(p -> p.toFile().getName().endsWith(".pdf"))
+                    .forEach(System.out::println);
         }
     }
+
     public static void walkDirs() throws Exception {
         Path path = Paths.get("G:", "University");
-        try(Stream<Path> stream = Files.walk(path)){
+        try (Stream<Path> stream = Files.walk(path)) {
             stream
-                  //.filter(p -> p.toFile().isDirectory())
-                  .forEach(System.out::println);
+                    //.filter(p -> p.toFile().isDirectory())
+                    .forEach(System.out::println);
         }
     }
-    
-    
+
     private static void streamsIntro() {
         BinaryOperator<Integer> result = (x, y) -> x + y;
 
@@ -129,24 +141,43 @@ public class Main {
 
     public static void showDirs(File dir) {
         FileFilter filter = (p) -> p.getName().endsWith(".java");
-        FilenameFilter nameFilter = (d ,n) -> n.endsWith(".java");
+        FilenameFilter nameFilter = (d, n) -> n.endsWith(".java");
 
         Arrays.asList(dir.list())
                 .forEach(System.out::println);
     }
-     
-    public static void consumerTests(){
-        
-        List<Integer> ints = Arrays.asList(new Integer[]{1,2,3,4,5,6,7,8,9,10});
+
+    public static void consumerTests() {
+
+        List<Integer> ints = Arrays.asList(new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
         List<Integer> result = new ArrayList<>();
-        
+
         Consumer<Integer> print = System.out::println;
         Consumer<Integer> add = result::add;
         Consumer<Integer> addAndShow = add.andThen(print);
-        
-        
-        
+
         ints.forEach(addAndShow);
         result.forEach(print);
+    }
+
+    public static void countWords(File file) throws Exception {
+        Pattern pattern = Pattern.compile("\\s+");
+
+        TreeMap<String, Long> wordsCount = Files.lines(file.toPath())
+                .map(line -> line.replaceAll("(?!')\\p{P}", ""))
+                .flatMap(line -> pattern.splitAsStream(line))
+                .collect(Collectors.groupingBy(String::toLowerCase,
+                                TreeMap::new, Collectors.counting()));
+
+        wordsCount.entrySet()
+                .stream()
+                .collect(Collectors.groupingBy(entry -> entry.getKey().charAt(0),
+                                TreeMap::new, Collectors.toList()))
+                .forEach((letter, wordList) -> {
+                    System.out.printf("%n%C%n", letter);
+                    wordList.stream()
+                    .forEach(word -> System.out.printf("%13s: %d%n", word.getKey(), word.getValue()));
+                });
+
     }
 }
